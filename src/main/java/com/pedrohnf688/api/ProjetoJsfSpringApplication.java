e959fc4cd3255ae30ff386c7a96dfde1f7cdf298
Bolsista <Bolsista>
2019-06-12 13:49:47 -0300

    first commit

---
+++ src/main/java/com/pedrohnf688/api/ProjetoJsfSpringApplication.java
@@ -0,0 +1,44 @@
+package com.pedrohnf688.api;
+
+import java.util.EnumSet;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.DispatcherType;
+
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan({ "com.pedrohnf688.api" })
+@SpringBootApplication
+public class ProjetoJsfSpringApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJsfSpringApplication.class, args);
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		FacesServlet servlet = new FacesServlet();
+		return new ServletRegistrationBean(servlet, "*.jsf");
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Bean
+	public FilterRegistrationBean rewriteFilter() {
+		FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
+		rwFilter.setDispatcherTypes(
+				EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR));
+		rwFilter.addUrlPatterns("/*");
+		return rwFilter;
+	}
+
+}
